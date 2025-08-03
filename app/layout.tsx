cc23731d52c1918bd82811bce02c11ef00d6eb42
Rody Huancas <rodyhuancas.04@gmail.com>
2025-08-02 23:43:37 -0500

    Feat: Agregar componente Navbar al layout principal para mejorar la navegación en la aplicación

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Navbar } from "@/components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,8 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Navbar />
+
           {children}
         </body>
       </html>
